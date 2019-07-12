cc2d09494101e4dc44ec2c1c5fa1ca9b3faeaf08
washucode <estherw8525@gmail.com>
2019-07-12 18:06:18 +0300

    add fuction calculateAkhan and declare variables UserDateInput and Gender

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+function calulateAkhan() {
+  var userDateInput = document.getElementById("inputDate").value;
 
+  var gender ;
+  if (document.getElementById('femaleChecked').checked) {
+   gender = document.getElementById('femaleChecked').value;
+   }
+   else if(document.getElementById('maleChecked').checked){
+     gender = document.getElementById('maleChecked').value;
+
+   }
